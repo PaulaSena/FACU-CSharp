859e0a096972d412ed72563c5f508f72206f3131
Paula Sena <senasilvaanapaula@gmail.com>
2021-03-10 22:36:31 -0300

    first commit atividades c#

---
+++ EX01C02/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace O1
+{
+    class EscritaMensagem
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bem-vindos ao C#");
+        }
+    }
+}
---
+++ EX02C02/Program.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace O2
+{
+    class IntercalaMsg
+    {
+        static void Main(string[] args) => Console.WriteLine("Caros {0} sejam {1} ao C#!", "alunos", "bem-vindos");
+    }
+}
